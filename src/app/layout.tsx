ab42b6d4b00495e3536c39f1b499ca916a00c5ff
Karel Busta <karelbusta@gmail.com>
2025-11-17 11:10:03 +0100

    add shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import ConvexClientProvider from '@/src/components/ConvexClientProvider';
+import ConvexClientProvider from '@/components/ConvexClientProvider';
 import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
