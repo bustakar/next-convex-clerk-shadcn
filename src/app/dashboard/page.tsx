09ca9799443a68f3cdf9710e66359bbe85e77d94
Karel Busta <karelbusta@gmail.com>
2025-11-17 11:42:09 +0100

    Add dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import { UserButton } from '@clerk/nextjs';
+
+export default function DashboardPage() {
+  return (
+    <>
+      <header className="sticky top-0 z-10 bg-background p-4 border-b-2 border-slate-200 dark:border-slate-800 flex flex-row justify-between items-center">
+        Convex + Next.js + Clerk
+        <UserButton />
+      </header>
+      <main className="p-8 flex flex-col gap-8">
+        <h1 className="text-4xl font-bold text-center">Dashboard</h1>
+      </main>
+    </>
+  );
+}
